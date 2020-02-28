5b745ff5dc536f2273e672258835a13a238d834d
carferben <carferben11@gmail.com>
2020-02-28 22:22:06 +0100

    testA_3

--- CAI/cai2/apartado_d/testA_3.py
+++ CAI/cai2/apartado_d/testA_3.py
@@ -1,23 +1,26 @@
 import threading
-import queue
  
-def isPar(count, cola):
+
+count = 0
+
+def isPar(lock):
+    global count
     for i in range(500000):
+        lock.acquire()
         if(i%2 == 0):
             count = count + 1
-    cola.put("Hay " + str(count) + " pares")
+        lock.release()
       
 def main():
-    count = 0
-    cola = queue.Queue()
-    hilo_1 = threading.Thread(target= isPar,args=(count, cola))
-    hilo_2 = threading.Thread(target= isPar,args=(count, cola))
+    lock = threading.Lock()
+    hilo_1 = threading.Thread(target= isPar,args=(lock,))
+    hilo_2 = threading.Thread(target= isPar,args=(lock,))
     hilo_1.start()
     hilo_2.start()
     hilo_1.join()
     hilo_2.join()
-    print(cola.get())
 
 if __name__ == "__main__":
     main()
+    print("Hay " + str(count) + " pares")
 
