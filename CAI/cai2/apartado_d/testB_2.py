4b17254d96ba8c88206d4e105d1f940952ad748c
Álvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2020-02-29 13:52:40 +0100

    Finish test2 from section b and d

--- CAI/cai2/apartado_d/testB_2.py
+++ CAI/cai2/apartado_d/testB_2.py
@@ -1,22 +1,34 @@
-import multiprocessing
+from multiprocessing import Process, Queue, JoinableQueue
+from queue import Empty
+import time
 
 
-def square_list(mylist, q):
+def square_list(to_do, done, id_process):
     """
     function to square a given list
     """
-    # append squares of mylist to queue
-    for num in mylist:
-        q.put(num * num)
+    while not to_do.empty():
+        try:
+            elem = to_do.get(timeout=10)
+            done.put(elem * elem)
+            print('%s * %s done by process %s' % (elem, elem, id_process))
+            to_do.task_done()
+        except Empty:
+            break
+    print('Consumer %s done' % id_process)
 
 
-def print_queue(q):
+def print_queue(to_do, done):
     """
     function to print queue elements
     """
-    print("Queue elements:")
-    while not q.empty():
-        print(q.get())
+    while not to_do.empty() or not done.empty():
+        try:
+            elem = done.get()
+            print(elem)
+            done.task_done()
+        except Empty:
+            break
     print("Queue is now empty!")
 
 
@@ -25,16 +37,24 @@ if __name__ == "__main__":
     mylist = [1, 2, 3, 4]
 
     # creating multiprocessing Queue
-    q = multiprocessing.Queue()
+    to_do = JoinableQueue()
+    [to_do.put(elem) for elem in mylist]
+
+    done = JoinableQueue()
 
     # creating new processes
-    p1 = multiprocessing.Process(target=square_list, args=(mylist, q))
-    p2 = multiprocessing.Process(target=print_queue, args=(q,))
+    c1 = Process(target=print_queue, args=(to_do, done))
+    c1.start()
+    p1 = Process(target=square_list, args=(to_do, done, 1))
+    p1.start()
+    p2 = Process(target=square_list, args=(to_do, done, 2))
+    p2.start()
 
     # running process p1 to square list
-    p1.start()
     p1.join()
 
     # running process p2 to get queue elements
-    p2.start()
     p2.join()
+
+    # running process p2 to get queue elements
+    c1.join()
