8feac648389dcafd84a2e7b3b78dfa53b972f156
Álvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2020-03-04 16:24:06 +0100

    Added integrity check and write in the log file in the case of failure

--- PAI/pai1/code/integrity_process.py
+++ PAI/pai1/code/integrity_process.py
@@ -1,22 +1,24 @@
+from datetime import *
 from PAI.pai1.code.hash_utilities import Hash
 from PAI.pai1.code.mac_utilities import Hmac
 
 
 class IntegrityProcess:
-    def __init__(self, hash_user, file, hash_server, user_token):
-        self.hash_user = hash_user
+    def __init__(self, file, hash_from_server, user_token):
         self.file = file
-        self.hash_server = hash_server
+        self.hash_from_server = hash_from_server
         self.user_token = user_token
+        self.log_file = '../logs/log'
 
     def check_integrity(self):
-        file = 'files\\file1'
+        hash_from_file = Hash(self.file).get_hash()
 
-        hash = Hash(file).get_hash()
-
-        integrity_hash_test = Hash(file, hash, None).compare_hash()
+        integrity_hash_test = Hash(None, hash_from_file, self.hash_from_server).compare_hash()
         if integrity_hash_test:
-            return Hmac(self.user_token, file, None).get_mac()
+            return [True, Hmac(self.user_token, self.file, None).get_mac()]
         else:
-            print('Error of hash integrity')
-            return False
+            f = open(self.log_file, "w")
+            now = datetime.now()
+            f.write("ERROR: %s\n Integrity test has been failure in the file %s" % (str(now), self.file))
+            f.close()
+            return [False, None]
--- PAI/pai1/code/tests/integrity_process_utilities_test.py
+++ PAI/pai1/code/tests/integrity_process_utilities_test.py
@@ -1,6 +1,5 @@
 from PAI.pai1.code.hash_utilities import Hash
 from PAI.pai1.code.integrity_process import IntegrityProcess
-from PAI.pai1.code.mac_utilities import Hmac
 import unittest
 
 
@@ -11,12 +10,22 @@ class TestIntegrity(unittest.TestCase):
         file1 = 'files/file1'
         file2 = 'files/file1'
 
-        hash1 = Hash(file1).get_hash()
         hash2 = Hash(file2).get_hash()
 
-        integrity_check = IntegrityProcess(hash1, file1, hash2, user_token).check_integrity()
+        integrity_check = IntegrityProcess(file1, hash2, user_token).check_integrity()
 
-        self.assertEqual(integrity_check, True)
+        self.assertEqual(integrity_check[0], True)
+
+    def test_integrity_false(self):
+        user_token = '1234567891011121314151617181921'
+        file1 = 'files/file1'
+        file2 = 'files/file2'
+
+        hash2 = Hash(file2).get_hash()
+
+        integrity_check = IntegrityProcess(file1, hash2, user_token).check_integrity()
+
+        self.assertEqual(integrity_check[0], False)
 
 
 if __name__ == '__main__':
