c3967d84f8279b6a08b813616a710b96d7df9d44
unknown <antoniosalvatperez@gmail.com>
2020-03-04 18:50:52 +0100

    added bst

---
+++ PAI/pai1/code/generatorTXT.py
@@ -0,0 +1,8 @@
+
+import random
+
+for i in range(0,1000):
+    nameFile = "file" + str(i)
+    #with open("files/copy.txt", "w") as file:
+    with open('files/%s.txt' % nameFile, 'w') as file:
+        file.write(str(i))
