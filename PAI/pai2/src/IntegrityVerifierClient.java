7a66153d0215db3cb188129609518aeeb610fb21
Antonio <antoniosalvatperez@gmail.com>
2020-03-20 01:14:11 +0100

    implemented verification

--- PAI/pai2/src/IntegrityVerifierClient.java
+++ PAI/pai2/src/IntegrityVerifierClient.java
@@ -9,72 +9,90 @@ import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.net.SocketFactory;
 import javax.swing.JOptionPane;
 
+
 public class IntegrityVerifierClient {
-    public IntegrityVerifierClient(){
-        try {
-            SocketFactory socketFactory = ( SocketFactory ) SocketFactory.getDefault();
-            Socket socket = (Socket) socketFactory.createSocket("localhost", 7070 );
-            PrintWriter output = new PrintWriter(new OutputStreamWriter( socket.getOutputStream() ) );
-            String mensaje = JOptionPane.showInputDialog(null,"Introduzca su mensaje:" );
+	
+	// Constructor que abre una conexión Socket para enviar mensaje/MAC al servidor
+	 public IntegrityVerifierClient() throws NoSuchAlgorithmException, InvalidKeyException{
+	 try {
+	 SocketFactory socketFactory = ( SocketFactory ) SocketFactory.getDefault();
+	 Socket socket = ( Socket ) socketFactory.createSocket("localhost", 7070 );
+	
+	 // Crea un PrintWriter para enviar mensaje/MAC al servidor
+	 PrintWriter output = new PrintWriter(new OutputStreamWriter( socket.getOutputStream() ) );
+	 String mensaje = JOptionPane.showInputDialog(null,"Introduzca su mensaje:" );
+	 
+	 // Envío del mensaje al servidor
+	 output.println(mensaje);
+	 
+	 String secreto = "secreto";
+	 
+	 final Mac mac_SHA256 = Mac.getInstance("HmacSHA256");
+	 
+	// generate a key from the generator
+     SecretKeySpec key = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
 
-            output.println(mensaje);
-            output.println(this.getMac(mensaje, "test"));
+     mac_SHA256.init(key);
 
-            String macdelMensaje = "";
-            output.println(macdelMensaje);
-            output.flush();
-            BufferedReader input = new BufferedReader(new InputStreamReader( socket.getInputStream ()) );
-            String respuesta = input.readLine();
-            JOptionPane.showMessageDialog( null, respuesta);
-            output.close();
-            input.close();
-            socket.close();
-        } 
-        catch ( IOException ioException ) {
-            ioException.printStackTrace();
-        }
-        finally {
-            System.exit( 0 );
-        }
-    }
-    // ejecución del cliente de verificación de la integridad
-    public static void main( String args[]){
-        new IntegrityVerifierClient();
-    }
-private String getMac(String mensaje, String secreto) {
-        try {
+     // get the string as UTF-8 bytes
+     byte[] b = mensaje.getBytes("UTF-8");
 
-            // get a key generator for the HMAC-SHA256 keyed-hashing algorithm
-            final Mac mac_SHA256 = Mac.getInstance("HmacSHA256");
-
-            // generate a key from the generator
-            SecretKeySpec key = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
-
-            mac_SHA256.init(key);
-
-            // get the string as UTF-8 bytes
-            byte[] b = mensaje.getBytes("UTF-8");
-
-            // create a digest from the byte array
-            byte[] digest = mac_SHA256.doFinal(b);
-
-            return null;
-
-        }catch (NoSuchAlgorithmException e) {
-            System.out.println("No Such Algorithm:" + e.getMessage());
-            return null;
-        }catch (UnsupportedEncodingException e) {
-            System.out.println("Unsupported Encoding:" + e.getMessage());
-            return null;
-        }catch (InvalidKeyException e) {
-            System.out.println("Invalid Key:" + e.getMessage());
-            return null;
-        }
-    }
+     // create a digest from the byte array
+     byte[] digest = mac_SHA256.doFinal(b);
+     
+     String digestHex = bytesToHex(digest);
+          	 
+	 // Habría que calcular el correspondiente MAC con la clave compartida por servidor/cliente
+	 output.println(digestHex);
+	 
+	 // Importante para que el mensaje se envíe
+	 output.flush();
+	 
+	 
+	 // Crea un objeto BufferedReader para leer la respuesta del servidor
+	 BufferedReader input = new BufferedReader(new InputStreamReader( socket.getInputStream ()) );
+	 
+	 // Lee la respuesta del servidor
+	 String respuesta = input.readLine();
+	
+	 // Muestra la respuesta al cliente
+	 JOptionPane.showMessageDialog( null, respuesta);
+	 
+	 // Se cierra la conexion
+	 output.close();
+	 input.close();
+	 socket.close();
+	 } 
+	 // end try
+	 catch ( IOException ioException ) {
+	 ioException.printStackTrace();
+	 }
+	
+	 // Salida de la aplicacion
+	 finally {
+	 System.exit( 0 );
+	 }
+	 }
+	 
+	 public static void main( String args[]) throws InvalidKeyException, NoSuchAlgorithmException{
+		 new IntegrityVerifierClient();
+		}
+	 
+	 private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+	 public static String bytesToHex(byte[] bytes) {
+	     char[] hexChars = new char[bytes.length * 2];
+	     for (int j = 0; j < bytes.length; j++) {
+	         int v = bytes[j] & 0xFF;
+	         hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+	         hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+	     }
+	     return new String(hexChars);
+	 }
 }
--- PAI/pai2/src/IntegrityVerifierServer.java
+++ PAI/pai2/src/IntegrityVerifierServer.java
@@ -7,34 +7,41 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.net.ServerSocketFactory;
 
 public class IntegrityVerifierServer {
+	
 	 private ServerSocket serverSocket;
 	 // Constructor del Servidor
-	 
+	
 	 public IntegrityVerifierServer() throws Exception {
+	 
 	 // ServerSocketFactory para construir los ServerSockets
 	 ServerSocketFactory socketFactory = ( ServerSocketFactory ) ServerSocketFactory.getDefault();
+	 
 	 // Creación de un objeto ServerSocket escuchando peticiones en el puerto 7070
 	 serverSocket = (ServerSocket ) socketFactory.createServerSocket(7070);
 	 }
 	 
 	 // Ejecución del servidor para escuchar peticiones de los clientes
-	 private void runServer(){
+	 private void runServer() throws InvalidKeyException, NoSuchAlgorithmException{
 	 while (true) {
 	
-	 // Espera las peticiones del cliente para comprobar mensaje/MAC
+	// Espera las peticiones del cliente para comprobar mensaje/MAC
 	 try {
 	 System.err.println( "Esperando conexiones de clientes...");
 	 Socket socket = ( Socket ) serverSocket.accept();
 	 
 	 // Abre un BufferedReader para leer los datos del cliente
 	 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	
+	 
 	 // Abre un PrintWriter para enviar datos al cliente
-	 PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream() ) );
+	PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream() ) );
 	 
 	// Se lee del cliente el mensaje y el macdelMensajeEnviado
 	 String mensaje = input.readLine();
@@ -43,23 +50,55 @@ public class IntegrityVerifierServer {
 	 String macdelMensajeEnviado = input.readLine();
 	 
 	 //mac del MensajeCalculado
-	 if (macdelMensajeEnviado.equals("")) {
+	 
+	 String secreto = "secreto";
+	 
+	 final Mac mac_SHA256 = Mac.getInstance("HmacSHA256");
+	 
+	 SecretKeySpec key = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
+
+	 mac_SHA256.init(key);
+
+	 // get the string as UTF-8 bytes
+	 byte[] b = mensaje.getBytes("UTF-8");
+
+	 // create a digest from the byte array
+	 byte[] digest = mac_SHA256.doFinal(b);
+	 
+	 
+	 String macDelMensajeCalculado = bytesToHex(digest);
+	     
+	 System.err.println( macdelMensajeEnviado);
+	     
+	 if (macdelMensajeEnviado.equals(macDelMensajeCalculado)) {
 	 output.println( "Mensaje enviado integro " );
 	 }
 	 else {
 	 output.println( "Mensaje enviado no integro." );
-	 } 
+	 }
 	 output.close();
 	 input.close();
 	 socket.close();
 	 }
 	 catch ( IOException ioException ) { ioException.printStackTrace(); }
 	 }
-	 } 
-
-	public static void main(String[] args)  throws Exception  {
-		IntegrityVerifierServer server = new IntegrityVerifierServer();
-		server.runServer();
-	}
+	 }
+	 
+	 public static void main( String args[] ) throws Exception
+	 {
+	 IntegrityVerifierServer server = new IntegrityVerifierServer();
+	 server.runServer();
+	 }
+	 
+	 private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+	 public static String bytesToHex(byte[] bytes) {
+	     char[] hexChars = new char[bytes.length * 2];
+	     for (int j = 0; j < bytes.length; j++) {
+	         int v = bytes[j] & 0xFF;
+	         hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+	         hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+	     }
+	     return new String(hexChars);
+	 }
 
 }
