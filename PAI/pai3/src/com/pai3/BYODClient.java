2e007275e78a1277c9757f2b6c9475bcd78f626a
Álvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2020-04-03 18:35:02 +0200

    [feat] Add PAI2 new

--- PAI/pai3/src/com/pai3/BYODClient.java
+++ PAI/pai3/src/com/pai3/BYODClient.java
@@ -1,147 +1,50 @@
 package com.pai3;
 
 import java.io.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.swing.JOptionPane;
-import java.util.Date;
-
 
 
 public class BYODClient {
 
-	public Utilities utilities = new Utilities();
-
-    // Constructor que abre una conexi�n Socket para enviar mensaje/MAC al servidor
-    public BYODClient() throws NoSuchAlgorithmException, InvalidKeyException {
+    public BYODClient() {
         try {
             SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
             SSLSocket socket = (SSLSocket) socketFactory.createSocket("localhost", 7070);
-
             // Crea un PrintWriter para enviar mensaje/MAC al servidor
             PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            String mensaje = JOptionPane.showInputDialog(null, "Introduzca su mensaje:");
-
-            // Env�o del mensaje al servidor
+            String userName = JOptionPane.showInputDialog(null, "Introduzca su mensaje:");
+            // Envío del mensaje al servidor
+            String mensaje = "Santa María del Alcor";
             output.println(mensaje);
-
-            // Clave compartida client - server
-            String secreto = "secreto";
-
-            // Construcci�n del Nonce
-            String nonce = Nonce.createRandomNonce();
-            output.println(nonce);
-
-            // generate a key from the generator
-            SecretKeySpec key = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
-
-            // Construcci�n de la MAC
-            final Mac mac_SHA256 = Mac.getInstance("HmacSHA256");
-            mac_SHA256.init(key);
-
-            String mensajeNonce = mensaje + nonce;
-
-            // get the string as UTF-8 bytes
-            byte[] b = mensajeNonce.getBytes("UTF-8");
-
-            // create a digest from the byte array
-            byte[] digest = mac_SHA256.doFinal(b);
-
-            String digestHex = utilities.bytesToHex(digest);
-
-            // Habr�a que calcular el correspondiente MAC con la clave compartida por servidor/cliente
-            output.println(digestHex);
-
-            // Importante para que el mensaje se env�e
+            // Habría que calcular el correspondiente MAC con la clave compartida por servidor/cliente
+            String macdelMensaje = "San Pedro Nolasco";
+            output.println(macdelMensaje);
+            // Importante para que el mensaje se envíe
             output.flush();
-
-//----------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------
             // Crea un objeto BufferedReader para leer la respuesta del servidor
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            // Se lee del servidor el mensaje y el macdelMensajeEnviado
-            String mensajeServer = input.readLine();
-
-            String nonceServer = input.readLine();
-
-            // A continuaci�n habr�a que calcular el mac del MensajeEnviado que podr�a ser
-            String macServer = input.readLine();
-
-            //mac del MensajeCalculado
-
-            final Mac mac_SHA256_server = Mac.getInstance("HmacSHA256");
-
-            SecretKeySpec key_verify = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
-
-            mac_SHA256_server.init(key_verify);
-
-            String mensajeNonceServer = mensajeServer + nonceServer;
-
-            // get the string as UTF-8 bytes
-            byte[] bServer = mensajeNonceServer.getBytes("UTF-8");
-
-            // create a digest from the byte array
-            byte[] digest_server = mac_SHA256_server.doFinal(bServer);
-
-
-            String macDelMensajeCalculado = utilities.bytesToHex(digest_server);
-
-            FileReader linesNonce = new FileReader("src/nonce_client.log");
-
-            Boolean nonceValid = true;
-
-            try (BufferedReader br = new BufferedReader(linesNonce)) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    if (line.equals(nonceServer)) {
-                        nonceValid = false;
-                    }
-                }
-            }
-
-            if (macServer.equals(macDelMensajeCalculado) && nonceValid) {
-                JOptionPane.showMessageDialog(null, mensajeServer);
-                
-                File fw = new File("src/nonce_client.log");
-                BufferedWriter bw = new BufferedWriter(new FileWriter(fw, true));
-                bw.append(nonce);
-                bw.newLine();
-                bw.close();
-                
-            } else {
-                JOptionPane.showMessageDialog(null, mensajeServer);
-
-                File fw = new File("src/logFile_client.log");
-                BufferedWriter bw = new BufferedWriter(new FileWriter(fw, true));
-                Date date = new Date();
-                bw.append("ERROR: " + date + "\n" + "Integrity message has been failure. Message: " + mensaje);
-                bw.newLine();
-                bw.close();
-            }
-
+            // Lee la respuesta del servidor
+            String respuesta = input.readLine();
+            // Muestra la respuesta al cliente
+            JOptionPane.showMessageDialog(null, respuesta);
             // Se cierra la conexion
             output.close();
             input.close();
             socket.close();
-        }
-        // end try
+        } // end try
         catch (IOException ioException) {
             ioException.printStackTrace();
         }
-
         // Salida de la aplicacion
         finally {
             System.exit(0);
         }
     }
 
-    public static void main(String args[]) throws InvalidKeyException, NoSuchAlgorithmException {
+    // ejecución del cliente de verificación de la integridad
+    public static void main(String args[]) {
         new BYODClient();
     }
-
 }
--- PAI/pai3/src/com/pai3/BYODServer.java
+++ PAI/pai3/src/com/pai3/BYODServer.java
@@ -1,163 +1,47 @@
 package com.pai3;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Date;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 
 public class BYODServer {
 
-    private SSLServerSocket serverSocket;
-    public Utilities utilities = new Utilities();
+    private ServerSocket serverSocket;
 
     // Constructor del Servidor
     public BYODServer() throws Exception {
-
         SSLServerSocketFactory socketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
         serverSocket = (SSLServerSocket) socketFactory.createServerSocket(7070);
-
     }
 
     // Ejecución del servidor para escuchar peticiones de los clientes
-    private void runServer() throws InvalidKeyException, NoSuchAlgorithmException {
+    private void runServer() {
         while (true) {
-
             // Espera las peticiones del cliente para comprobar mensaje/MAC
             try {
                 System.err.println("Esperando conexiones de clientes...");
                 Socket socket = (Socket) serverSocket.accept();
-
                 // Abre un BufferedReader para leer los datos del cliente
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
                 // Abre un PrintWriter para enviar datos al cliente
                 PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-
                 // Se lee del cliente el mensaje y el macdelMensajeEnviado
                 String mensaje = input.readLine();
-
-                String nonce = input.readLine();
-
                 // A continuación habría que calcular el mac del MensajeEnviado que podría ser
                 String macdelMensajeEnviado = input.readLine();
-
                 //mac del MensajeCalculado
 
-                String secreto = "secreto";
-
-                final Mac mac_SHA256 = Mac.getInstance("HmacSHA256");
-
-                SecretKeySpec key = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
-
-                mac_SHA256.init(key);
-
-                String mensajeNonce = mensaje + nonce;
-
-                // get the string as UTF-8 bytes
-                byte[] b = mensajeNonce.getBytes("UTF-8");
-
-                // create a digest from the byte array
-                byte[] digest = mac_SHA256.doFinal(b);
-
-
-                String macDelMensajeCalculado = utilities.bytesToHex(digest);
-
-                FileReader linesNonce = new FileReader("src/nonce.log");
-
-                Boolean nonceValid = true;
-
-                try (BufferedReader br = new BufferedReader(linesNonce)) {
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        if (line.equals(nonce)) {
-                            nonceValid = false;
-                        }
-                    }
-                }
-                if (macdelMensajeEnviado.equals(macDelMensajeCalculado) && nonceValid) {
-                    output.println("Mensaje enviado integro");
-                    File fw = new File("src/nonce.log");
-                    BufferedWriter bw = new BufferedWriter(new FileWriter(fw, true));
-                    bw.append(nonce);
-                    bw.newLine();
-                    bw.close();
-                    // Construcción del Nonce
-                    String nonce_pass = Nonce.createRandomNonce();
-                    output.println(nonce_pass);
-
-                    // generate a key from the generator
-                    SecretKeySpec key_pass = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
-
-                    // Construcción de la MAC
-                    final Mac mac_SHA256_pass = Mac.getInstance("HmacSHA256");
-                    mac_SHA256_pass.init(key_pass);
-
-                    String mensajeNonce_pass = "Mensaje enviado integro" + nonce_pass;
-
-                    // get the string as UTF-8 bytes
-                    byte[] b_pass = mensajeNonce_pass.getBytes("UTF-8");
-
-                    // create a digest from the byte array
-                    byte[] digest_pass = mac_SHA256_pass.doFinal(b_pass);
-
-                    String digestHex_pass = utilities.bytesToHex(digest_pass);
-
-                    // Habría que calcular el correspondiente MAC con la clave compartida por servidor/cliente
-                    output.println(digestHex_pass);
-
-                    // Importante para que el mensaje se envíe
-                    output.flush();
-
+//                if (macMensajeEnviado.equals(macdelMensajeCalculado)) {
+                if (true) {
+                    output.println("Mensaje enviado integro ");
                 } else {
                     output.println("Mensaje enviado no integro.");
-
-                    File fw = new File("src/logFile.log");
-                    BufferedWriter bw = new BufferedWriter(new FileWriter(fw, true));
-                    Date date = new Date();
-                    bw.append("ERROR: " + date + "\n" + "Integrity message has been failure. Message: " + mensaje);
-                    bw.newLine();
-                    bw.close();
-
-                    // Construcción del Nonce
-                    String nonce_pass = Nonce.createRandomNonce();
-                    output.println(nonce_pass);
-
-                    // generate a key from the generator
-                    SecretKeySpec key_pass = new SecretKeySpec(secreto.getBytes(), "HmacSHA256");
-
-                    // Construcción de la MAC
-                    final Mac mac_SHA256_pass = Mac.getInstance("HmacSHA256");
-                    mac_SHA256_pass.init(key_pass);
-
-                    String mensajeNonce_pass = "Mensaje enviado no integro." + nonce_pass;
-
-                    // get the string as UTF-8 bytes
-                    byte[] b_pass = mensajeNonce_pass.getBytes("UTF-8");
-
-                    // create a digest from the byte array
-                    byte[] digest_pass = mac_SHA256_pass.doFinal(b_pass);
-
-                    String digestHex_pass = utilities.bytesToHex(digest_pass);
-
-                    // Habría que calcular el correspondiente MAC con la clave compartida por servidor/cliente
-                    output.println(digestHex_pass);
-
-                    // Importante para que el mensaje se envíe
-                    output.flush();
                 }
                 output.close();
                 input.close();
@@ -168,6 +52,7 @@ public class BYODServer {
         }
     }
 
+    // Programa principal
     public static void main(String args[]) throws Exception {
         BYODServer server = new BYODServer();
         server.runServer();
